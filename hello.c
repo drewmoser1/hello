eb5a93b8bd056e4bf8f8d64e4483ea287250334b
Drew Moser <dmoser4@huskers.unl.edu>
2024-08-29 15:56:36 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Drew Moser
+ * Date: 2024/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Computer Engineering\n");
+  printf("Drew Moser!\n");
+
+  return 0;
+}
