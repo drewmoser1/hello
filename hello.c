54a6f270b5fb18ffcbcc458412e6abc9c7184afc
Alexander Voss <avoss14@huskers.unl.edu>
2024-08-30 22:30:46 -0500

    added my name and major

--- hello.c
+++ hello.c
@@ -13,5 +13,8 @@ int main(int argc, char **argv) {
   printf("Hello World, I'm Drew Moser!\n");
   printf("My major is Computer Engineering!\n");
 
+  printf("Computer Engineering\n");
+  printf("Alex Voss\n");
+  
   return 0;
 }
